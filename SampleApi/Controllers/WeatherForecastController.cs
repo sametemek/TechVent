9ef40415601bd781bd9e744e0bb08465fbfd3246
Samet Emek <samet.emek@inventiv.com.tr>
2021-03-10 09:52:35 +0300

    updated.

--- SampleApi/Controllers/WeatherForecastController.cs
+++ SampleApi/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace SampleApi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Blabla"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
